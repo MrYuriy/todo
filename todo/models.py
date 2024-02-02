a6213a90c25b2aa6da9030146148a5e423555455
MrYuriy <yuriybabiyk@gmail.com>
2024-02-02 21:44:12 +0100

    Create Todo and Tag models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+# Create Tag model
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    tag = models.ManyToManyField(Tag)
+    completed = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) -> str:
+        return self.title
